579522000205e65038cee06b739478def037d946
metamystik <br.levio@duocuc.cl>
2026-04-29 13:09:43 -0400

    feat: creación de estructura monorepo y servicio académico

---
+++ academico-service/academico-service/src/main/java/com/ev2/academico/AcademicoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ev2.academico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicoServiceApplication.class, args);
+	}
+
+}
---
+++ academico-service/academico-service/src/test/java/com/ev2/academico/AcademicoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ev2.academico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
